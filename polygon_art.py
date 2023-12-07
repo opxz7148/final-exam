998b78f86c45013a3a565d494dc04ccd02799eff
Pasu Sangiemsin <opxz7148@gmail.com>
2023-12-07 09:30:38 +0700

    Implement class for canva and each polygon

--- polygon_art.py
+++ polygon_art.py
@@ -1,6 +1,43 @@
 import turtle
 import random
 
+# specify a reduction ratio to draw a smaller polygon inside the one above
+
+reduction_ratio = 0.618
+
+class Canva:
+
+    def __init__(self):
+        self.choice = int(input("Which art do you want to generate? Enter a number between 1 to 8, inclusive: "))
+
+
+class Polygon:
+
+    def __init__(self, num_sides, size, orientation, location, color, border_size, level=1):
+        self.num_sides = num_sides
+        self.size = size
+        self.orien = orientation
+        self.location = location
+        self.color = color
+        self.border_size = border_size
+        self.level = level
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.location[0], self.location[1])
+        turtle.setheading(self.orien)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
+        turtle.pendown()
+        for _ in range(self.num_sides):
+            turtle.forward(self.size)
+            turtle.left(360 / self.num_sides)
+        turtle.penup()
+
+    def reduction(self):
+
+
+
 def draw_polygon(num_sides, size, orientation, location, color, border_size):
     turtle.penup()
     turtle.goto(location[0], location[1])
@@ -15,7 +52,7 @@ def draw_polygon(num_sides, size, orientation, location, color, border_size):
 
 def get_new_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
+#
 turtle.speed(0)
 turtle.bgcolor('black')
 turtle.tracer(0)
@@ -30,8 +67,7 @@ color = get_new_color()
 border_size = random.randint(1, 10)
 draw_polygon(num_sides, size, orientation, location, color, border_size)
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
+
 
 # reposition the turtle and get a new location
 turtle.penup()
@@ -45,8 +81,13 @@ location[1] = turtle.pos()[1]
 # adjust the size according to the reduction ratio
 size *= reduction_ratio
 
-# draw the second polygon embedded inside the original 
+# draw the second polygon embedded inside the original
 draw_polygon(num_sides, size, orientation, location, color, border_size)
 
 # hold the window; close it by clicking the window close 'x' mark
+
+
+poly1 = Polygon(4,100, 60, [122, -150], get_new_color(), 3)
+poly1.draw()
+
 turtle.done()
